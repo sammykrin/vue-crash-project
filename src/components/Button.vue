2906d9ee8837aaeb73adec28c7b0115f9f6e8830
sammykrin <olamilehinsamuelolamide@gmail.com>
2023-12-09 22:25:31 -0800

    added btn settings

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,10 +1,19 @@
 <template>
-    <button class='btn'>Add Task</button>
+    <button @click="onClick()" :style="  { background: color }" class='btn'>{{text}}</button>
 </template>
 
 <script>
 export default {
     name:'Button',
+    props:{
+        text:String,
+        color:String,
+    },
+    methods:{
+        onClick(){
+            console.log('click');
+        }
+    }
 }
 </script>
 
