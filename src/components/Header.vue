e87e1439a63b755a06a9927920fbdd72180d1a81
sammykrin <olamilehinsamuelolamide@gmail.com>
2023-12-09 21:31:00 -0800

    added btn

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,20 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
+        <Button />
     </header>
 </template>
 
 <script>
+import Button from './Button'
+
 export default {
     name:'Header',
     props:{
         title:String,
+    },
+    components:{
+        Button
     }
 }
 </script>
